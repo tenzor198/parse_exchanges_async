1097bbfb8b47cecdfbe00984b0330ddaf89c8d02
tenzor198 <daur.abishev98@gmail.com>
2022-11-08 13:11:43 +0300

    add files

--- currency.py
+++ currency.py
@@ -57,6 +57,7 @@ async def corona_curs(currency):
     async with aiohttp.ClientSession() as session:
         async with session.get('https://koronapay.com/transfers/online/api/transfers/tariffs', params=params, headers=headers) as resp:
             result = await resp.json(content_type='text/html')
+            print(result)
             return result[0]['exchangeRate']
 
 
